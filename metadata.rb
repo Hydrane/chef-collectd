fbd206e2cc0f2cfe3bed9c5cc399b287bbe5f61b
Robert Vežnaver <rveznaver@users.noreply.github.com>
2017-08-07 11:20:40 +0200

    Verion bump

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'chef-collectd'
-version          '0.0.1'
+version          '0.0.2'
 license          'Apache 2.0'
 maintainer       'Robert Veznaver'
 maintainer_email 'rv@bidmotion.com'
